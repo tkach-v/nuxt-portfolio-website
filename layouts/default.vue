152788e0dffa99bbaceb01dcbfcc31462f970ca3
Volodymyr Tkach <vovantkac5@gmail.com>
2024-08-27 08:48:58 +0300

    add tailwind

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,18 +1,30 @@
 <template>
-  <div>
-    <nav>
-      <ul>
-        <li>
-          <NuxtLink to="/">Main</NuxtLink>
-        </li>
-        <li>
-          <NuxtLink to="/about">About</NuxtLink>
-        </li>
-        <li>
-          <NuxtLink to="/blog">Blog</NuxtLink>
-        </li>
-      </ul>
-    </nav>
-    <slot />
+  <div class="container mx-auto">
+    <header class="flex justify-between items-center mt-5">
+      <div>
+        <NuxtLink
+          to="/"
+          class="text-xl font-semibold p-2 hover:bg-gray-200"
+        >
+          Username
+        </NuxtLink>
+      </div>
+      <nav class="font-mono">
+        <ul class="flex space-x-5">
+          <li>
+            <NuxtLink to="/about">About</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/blog">Blog</NuxtLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+    <main class="p-2">
+      <slot/>
+    </main>
+
   </div>
 </template>
+<script setup lang="ts">
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
+  modules: [
+    '@nuxtjs/tailwindcss'
+  ],
 })
